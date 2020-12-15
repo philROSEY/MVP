91031bff30e705a85819c575f20e5b86bbc7cc16
Philip <philip.rosenberg@comcast.netgit config --global core.editor vim>
2020-12-15 14:57:50 -0500

    Added itemId.js, adjusted state in items, started axios request

--- src/items/items.js
+++ src/items/items.js
@@ -5,7 +5,14 @@ import heroIdObj from '../../heroIds.js'
 class ItemStats extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {};
+        this.state = {
+            teamA: [this.props.playerHero, this.props.teammate1, this.props.teammate2, this.props.teammate3, this.props.teammate4],
+            teamB: [this.props.enemyHero1, this.props.enemyHero2, this.props.enemyHero3, this.props.enemyHero4, this.props.enemyHero5]
+        };
+    }
+
+    findMatches(team1, team2) {
+        axios.get(`https://api.opendota.com/api/findmatches?api_key=33be2e86-1af6-40f5-8113-0fa8430369d7&teamA=[]=${team1[0]}, ${team1[1]}, ${team1[2]}, ${team1[3]}, ${team1[4]}&teamB=[]=${team2[0]}, ${team2[1]}, ${team2[2]}, ${team2[3]}, ${team2[4]} `)
     }
 
 
